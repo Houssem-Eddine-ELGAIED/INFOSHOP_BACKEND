88b1e89735635d19572d35e573ed87349226bac0
Houssem Eddine ELGAIED <houssemelgaied@gmail.com>
2024-12-16 09:44:39 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import productRouter from './routes/productRoutes.js';
 import userRouter from './routes/userRoutes.js';
 import orderRouter from './routes/orderRoutes.js';
 import uploadRouter from './routes/uploadRoutes.js';
-import cors from 'cors'
+
 
 
 dotenv.config();
@@ -26,10 +26,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
- app.use(cors({
-     origin: 'https://shopinfo.onrender.com', // L'URL de votre frontend
-     methods: ['GET', 'POST'],
- }));
+
 
 // app.get('/api/keys/paypal', (req, res) => {
 //   res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
